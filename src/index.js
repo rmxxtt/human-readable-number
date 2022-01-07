b9904c111f3257de5c0ba1ceb680609352add8cd
rmxxtt <62797019+rmxxtt@users.noreply.github.com>
2022-01-07 22:49:24 +0700

    feat: add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function toReadable (number) {
-  
+// module.exports = function toReadable (number) {
+//
+// }
+
+//module.exports = function toReadable (number) {
+function toReadable(number) {
+    let result = '';
+
+    const names = [
+        ['zero', 'one', 'two', 'three',
+            'four', 'five', 'six', 'seven', 'eight', 'nine']
+        ,
+        ['', 'ten', 'twenty', 'thirty', 'forty',
+            'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+        ,
+        ['hundred']
+    ];
+
+    number.toString().split('').reverse().map((value, index, array) => {
+        const n = parseInt(value);
+        if (index >= 2) { // сотни
+            result = `${names[0][n]} ${names[index][0]}${result ? ' ' + result : ''}`;
+        } else {
+            result = `${names[index][n]}${result ? ' ' + result : ''}`;
+        }
+
+        console.log("result", index, n, result)
+    });
+
+    // Удаляем лишние zero
+    if (result.length > 1000) {
+        result = result.replace(/zero/, '');
+    }
+
+    return result;
 }
+
+console.log(toReadable(101))
+// 125 +
